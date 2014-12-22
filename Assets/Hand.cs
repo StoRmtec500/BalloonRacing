874b87119972af935797e4dc11f9867c29691a14
StoRmtec500 <martin@kuenz.co.at>
2014-12-22 15:50:40 +0100

    1st Init

---
+++ Assets/Hand.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hand : MonoBehaviour {
+
+    public float moveSpeed = 0.02f;
+    public float currentPos;
+    public bool left = false;
+
+	// Use this for initialization
+	void Start () {
+        currentPos = transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!left)
+        {
+            transform.position -= new Vector3(moveSpeed, 0, 0);
+        }
+        else
+        {
+            transform.position += new Vector3(moveSpeed, 0, 0);
+        }
+
+        if (transform.position.x <= currentPos - 0.7f)
+        {
+            moveSpeed *= -1;
+        }
+        if (transform.position.x >= currentPos + 0.7f)
+        {
+            moveSpeed *= -1;
+        }
+	}
+}
---
+++ Assets/Kuh.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kuh : MonoBehaviour {
+
+    public Vector3 gravity = new Vector3(0, 0.02f, 0);
+    public GameObject micVolume;
+    public float moveSpeed;
+    public byte balloons = 3;
+    public GameObject alertWin;
+    public GameObject alterLos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        moveSpeed = micVolume.GetComponent<MicrophoneInput>().loudness * 0.01f;
+        transform.position = new Vector3(0, transform.position.y + moveSpeed, 0);
+
+        transform.position -= gravity;
+    }
+}
